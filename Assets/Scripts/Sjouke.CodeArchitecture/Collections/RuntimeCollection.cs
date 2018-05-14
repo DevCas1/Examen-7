6eb671f602b76347b7344a085ec9116591ee3319
Sjouke Yntema <suyntema@live.nl>
2018-05-14 12:29:46 +0200

    Unity project setup | Import CodeArchitecture v1.1 (NET 4.6) package

---
+++ Assets/Scripts/Sjouke.CodeArchitecture/Collections/RuntimeCollection.cs
@@ -0,0 +1,21 @@
+﻿namespace Sjouke.CodeArchitecture.RuntimeCollections
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    /// <summary>[CreateAssetMenu(menuName = "Runtime Collections/{YourType}\tCollection")]</summary>
+    public abstract class RuntimeCollection<T> : ScriptableObject 
+    {
+        public List<T> Items = new List<T>();
+
+        public void Add(T thing)
+        {
+            if (!Items.Contains(thing)) Items.Add(thing);
+        }
+
+        public void Remove(T thing)
+        {
+            if (Items.Contains(thing)) Items.Remove(thing);
+        }
+    }
+}
