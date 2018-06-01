0a20ab1406abb607ab0daa3e3fd9ea8cf045f418
Sjouke Yntema <suyntema@live.nl>
2018-06-01 10:25:09 +0200

    Remove redundant code

--- Assets/Scripts/Saving/SaveGameManager.cs
+++ Assets/Scripts/Saving/SaveGameManager.cs
@@ -51,7 +51,7 @@ namespace Sjouke.Serialization
                 using (FileStream file = File.Open(Application.persistentDataPath + _saveFileString, FileMode.Open))
                 {
                     Save save = FromJSONSave((string)formatter.Deserialize(file));
-                    playerLibrary.Cards = FromCardData(playerLibrary, save.PlayerLibrary);
+                    playerLibrary.Cards = FromCardData(save.PlayerLibrary);
                     gold.Value = save.Gold;
                 }
                 if (OnLoad != null) OnLoad.Raise();
@@ -82,7 +82,7 @@ namespace Sjouke.Serialization
                 FileStream file = File.Open(Application.persistentDataPath + _saveFileString, FileMode.Open);
                 Save save = FromJSONSave((string)formatter.Deserialize(file));
                 file.Close();
-                playerLibrary.Cards = FromCardData(playerLibrary, save.PlayerLibrary);
+                playerLibrary.Cards = FromCardData(save.PlayerLibrary);
                 gold.Value = save.Gold;
                 if (OnLoad != null) OnLoad.Raise();
             }
@@ -106,7 +106,7 @@ namespace Sjouke.Serialization
             return datas;
         }
 
-        internal List<PlayCardInfo> FromCardData(CardLibrary library, List<CardData> cardDatas)
+        internal List<PlayCardInfo> FromCardData(List<CardData> cardDatas)
         {
             List<PlayCardInfo> datas = new List<PlayCardInfo>();
             foreach (var card in cardDatas)
